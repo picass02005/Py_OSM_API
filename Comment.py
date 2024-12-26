02be936cb94e004aa1452d89bca673f22a13da6b
picasso2005 <clementduran0@gmail.com>
2024-12-26 01:29:02 +0100

    Added comment object

---
+++ Comment.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2024 picasso2005 <clementduran0@gmail.com> - All Rights Reserved
+
+from datetime import datetime
+from typing import Dict
+
+
+class OSMComment:
+    """
+    This class is used to represent a comment
+    """
+
+    def __init__(self, json_response: Dict[str: object]) -> None:
+        """
+        :param json_response: The json response of one comment
+        """
+
+        self.date: datetime = datetime.fromisoformat(json_response['date'])
+        self.action: str = json_response['action']
+        self.text: str = json_response['text']
+        self.html: str = json_response['html']
+
+        self.uid: int = json_response["uid"] if "uid" in json_response.keys() else -1
+        self.user: str = json_response["user"] if "user" in json_response.keys() else ""
+        self.user_url: str = json_response["user_url"] if "user_url" in json_response.keys() else ""
+
+    def __str__(self) -> str:
+        """
+        :return: A string corresponding to this object
+        """
+
+        return f"<OSMComment object: {self.user}, UID={self.uid}, Commented on {self.date.isoformat()}, {self.text}>"
